bacc229d2c99518a7d3a1bd7dcf9aa5eb3fb1211
Mike Schiessl <mschiess@akamai.com>
2022-08-24 12:27:40 +0200

    first working tests

---
+++ test.py
@@ -0,0 +1,34 @@
+import sys
+import sys
+import os
+
+token=os.environ.get('LN_TOKEN')
+
+import requests
+import time
+from datetime import datetime
+#dt_utcnow = datetime.datetime.utcnow()
+#print(dt_utcnow)
+
+ts = int(time.time()) - 6000
+timestamp = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%dT%H:%M:%S')
+print(timestamp)
+
+
+#https://www.linode.com/docs/api/#filtering-and-sorting
+my_filter = {'status': 'finished'}
+
+time = time.time()
+
+
+my_headers={
+    'Authorization': 'Bearer ' + token,
+    'X-FILTER': '{"created": {"+gte": "' + timestamp + '"}}'
+}
+
+
+print(my_headers)
+r = requests.get(url="https://api.linode.com/v4/account/events", headers=my_headers)
+print(r.headers)
+print("\n\n\n")
+print(r.json())
