74280908cbef147a4435dfacda222e9e31d9c684
Mike Schiessl <mschiess@akamai.com>
2024-10-04 16:45:31 +0200

    added stopEvent handler into events loop as well

--- bin/modules/events.py
+++ bin/modules/events.py
@@ -3,25 +3,25 @@ import sys
 import time
 from datetime import datetime
 import json
+import threading
 
 import modules.aka_log as aka_log
 import modules.generic as generic
 import ln_config.default_config as default_config
 
 
-def get_log(given_args=None, ln_edgerc=None, config_lopp_time=None, config_log_delay=None, config_page_size=500, route='/', params={}):
+def get_log(given_args=None, ln_edgerc=None, config_lopp_time=None, config_log_delay=None, config_page_size=500, route='/', params={}, stop_event: threading.Event=None):
     starttime = given_args.event_starttime - config_log_delay - config_lopp_time
     endtime = given_args.event_endtime - config_log_delay
     follow_mode = given_args.event_follow
     user_agent = given_args.ln_user_agent_prefix
 
-
-    while True:
+    while not stop_event.is_set():
         aka_log.log.debug(f"Request starttime: {starttime}, Endtime: {endtime}, follow mode: {follow_mode}")
-
+        tick = time.time()
 
         ts_starttime = datetime.utcfromtimestamp(starttime).strftime('%Y-%m-%dT%H:%M:%S')
-        print(f"ts_starttime: {ts_starttime}")
+        #print(f"ts_starttime: {ts_starttime}")
         #ts_starttime = datetime.datetime.formtimestamp(starttime, datetime.UTC)
         ts_endtime = datetime.utcfromtimestamp(endtime).strftime('%Y-%m-%dT%H:%M:%S')
 
@@ -40,7 +40,7 @@ def get_log(given_args=None, ln_edgerc=None, config_lopp_time=None, config_log_d
             my_params['page_size'] = config_page_size
 
             my_result = generic.api_request(method="GET", scheme="https://", url=ln_edgerc['linode_hostname'], path=route, params=my_params, headers=my_headers, payload=None, user_agent=user_agent)
-            print(f"my_result: {my_result}")
+            #print(f"my_result: {my_result}")
             for line in my_result['data']:
                 print(json.dumps(line))
 
@@ -52,16 +52,18 @@ def get_log(given_args=None, ln_edgerc=None, config_lopp_time=None, config_log_d
         if follow_mode:
             starttime = endtime
             endtime = endtime + config_lopp_time
-            time.sleep(config_lopp_time)
+            stop_event.wait(config_lopp_time - (time.time() - tick))
+            #time.sleep(config_lopp_time)
         else:
             break
 
-def audit(given_args=None, ln_edgerc=None):
-    get_log(given_args=given_args,
-            ln_edgerc=ln_edgerc,
-            config_lopp_time=default_config.audit_loop_time,
-            config_log_delay=default_config.audit_log_delay,
-            config_page_size=default_config.audit_page_size,
-            route="/v4/account/events")
+def audit(given_args=None, ln_edgerc=None, stop_event: threading.Event=None):
+        get_log(given_args=given_args,
+                ln_edgerc=ln_edgerc,
+                config_lopp_time=default_config.audit_loop_time,
+                config_log_delay=default_config.audit_log_delay,
+                config_page_size=default_config.audit_page_size,
+                stop_event=stop_event,
+                route="/v4/account/events")
 
 # EOF
